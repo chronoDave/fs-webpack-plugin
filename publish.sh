b5d858a013362a0c08c1eee34968f9b3790d0633
chronoDave <davidwolters6758@gmail.com>
2020-10-17 17:43:25 +0200

    minor: Publish update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-REPOSITORY=$(awk '/repository/{gsub(/("|",)/,"",$4);print $4}' package.json)
+REPOSITORY=$(node.exe -p -e "require('./package.json').repository")
 
 # yarn publish --non-interactive --new-version $3
+
 git remote set-url origin https://${GH_TOKEN}@${REPOSITORY} >/dev/null 2>&1
 git push origin master --tags
