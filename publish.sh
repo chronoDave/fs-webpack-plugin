1b1bae8ff86381d6656193cb320c7184611f6be9
chronoDave <davidwolters6758@gmail.com>
2020-10-17 17:16:00 +0200

    minor: Dangerously set values

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY=$(awk '/repository/{gsub(/("|",)/,"",$4);print $4}' package.json)
 
-yarn config set _authToken $1
+yarn config set _authToken $1 >/dev/null 2>&1
 yarn publish --non-interactive --new-version $3
-git remote set-url origin https://$2@$REPOSITORY
+git remote set-url origin https://${2}@${REPOSITORY} >/dev/null 2>&1
 git push origin master --tags
