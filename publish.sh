7f902e3bd6873b84055b8b3b3996a5da5a066e15
chronoDave <davidwolters6758@gmail.com>
2020-10-17 18:00:38 +0200

    minor: Bash magic

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 REPOSITORY=$(node -p -e "require('./package.json').repository")
+REPOSITORY=${REPOSITORY##*https://github.com/}
+
+ORIGIN="https://"
+ORIGIN+=$GH_TOKEN
+ORIGIN+=@
+ORIGIN+=$REPOSITORY
 
 # yarn publish --non-interactive --new-version $3
 
