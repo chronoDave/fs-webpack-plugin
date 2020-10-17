aaba5abffe3d63eb0e04f000524a0351b51cd53a
chronoDave <davidwolters6758@gmail.com>
2020-10-17 17:23:33 +0200

    minor: Ignore publish

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 
 REPOSITORY=$(awk '/repository/{gsub(/("|",)/,"",$4);print $4}' package.json)
 
-yarn publish --non-interactive --new-version $3
+# yarn publish --non-interactive --new-version $3
 git remote set-url origin https://${GH_TOKEN}@${REPOSITORY} >/dev/null 2>&1
 git push origin master --tags
