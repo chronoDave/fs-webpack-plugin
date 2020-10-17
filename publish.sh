f0ae985f915c90dcaf587d24ff185d358937ba80
chronoDave <davidwolters6758@gmail.com>
2020-10-17 17:10:14 +0200

    minor: Removed rc

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-yarn publish --non-interactive --new-version $1
+REPOSITORY=$(awk '/repository/{gsub(/("|",)/,"",$4);print $4}' package.json)
+
+yarn config set _authToken $1
+yarn publish --non-interactive --new-version $3
+git remote set-url origin https://$2@$REPOSITORY
 git push origin master --tags
