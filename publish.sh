b09f8fbfbc8755bfc7244222cf0345871a21e780
chronoDave <davidwolters6758@gmail.com>
2020-10-17 17:23:04 +0200

    minor: Don't use dangerous tokens

--- publish.sh
+++ publish.sh
@@ -2,7 +2,6 @@
 
 REPOSITORY=$(awk '/repository/{gsub(/("|",)/,"",$4);print $4}' package.json)
 
-yarn config set _authToken $1 >/dev/null 2>&1
 yarn publish --non-interactive --new-version $3
-git remote set-url origin https://${2}@${REPOSITORY} >/dev/null 2>&1
+git remote set-url origin https://${GH_TOKEN}@${REPOSITORY} >/dev/null 2>&1
 git push origin master --tags
